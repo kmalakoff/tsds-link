kmalakoff/tsds-mocha

Fix broken types #3

test/unit/lib.test.ts
@@ -32,12 +32,12 @@ function addTests(repo: string) {
     const deps = { ...(modulePackage.dependencies || {}), ...(modulePackage.peerDependencies || {}) };
 
     before((cb) => {
-      installGitRepo(repo, dest, (err?: Error): void => {
+      installGitRepo(repo, dest, (err?: Error | null): void => {
         if (err) return cb(err);
 
         const queue = new Queue(1);
-        queue.defer(linkModule.bind(null, modulePath, nodeModules));
-        for (const dep in deps) queue.defer(linkModule.bind(null, path.dirname(resolveSync(`${dep}/package.json`)), nodeModules));
+        queue.defer((cb) => linkModule(modulePath, nodeModules, (err) => cb(err)));
+        for (const dep in deps) queue.defer((cb) => linkModule(path.dirname(resolveSync(`${dep}/package.json`)), nodeModules, (err) => cb(err)));
         // Build the test repo
         queue.defer(spawn.bind(null, 'npm', ['run', 'build'], { cwd: dest, stdio: 'inherit' }));
         queue.await(cb);
@@ -45,14 +45,14 @@ function addTests(repo: string) {
     });
     after((cb) => {
       const queue = new Queue();
-      queue.defer(unlinkModule.bind(null, modulePath, nodeModules));
-      for (const dep in deps) queue.defer(unlinkModule.bind(null, path.dirname(resolveSync(`${dep}/package.json`)), nodeModules));
+      queue.defer((cb) => unlinkModule(modulePath, nodeModules, (err) => cb(err)));
+      for (const dep in deps) queue.defer((cb) => unlinkModule(path.dirname(resolveSync(`${dep}/package.json`)), nodeModules, (err) => cb(err)));
       queue.await(cb);
     });
 
     describe('happy path', () => {
       it('test:node', (done) => {
-        mocha([], { cwd: dest }, (err?: Error): void => {
+        mocha([], { cwd: dest }, (err?: Error | null): void => {
           if (err) return done(err);
 
           done();

kmalakoff/tsds-link

test/unit/lib.test.ts
@@ -22,7 +22,7 @@ const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : 
 
 const GITS = ['https://github.com/kmalakoff/fetch-http-message.git'];
 
-function addTests(repo) {
+function addTests(repo: string) {
   const repoName = path.basename(repo, path.extname(repo));
   describe(repoName, () => {
     const dest = path.join(tmpdir(), 'tsds-link', shortHash(process.cwd()), repoName);
@@ -32,32 +32,27 @@ function addTests(repo) {
     const deps = { ...(modulePackage.dependencies || {}), ...(modulePackage.peerDependencies || {}) };
 
     before((cb) => {
-      installGitRepo(repo, dest, (err?: Error): void => {
-        if (err) {
-          cb(err);
-          return;
-        }
+      installGitRepo(repo, dest, (err?: Error | null): void => {
+        if (err) return cb(err);
 
         const queue = new Queue();
-        queue.defer(linkModule.bind(null, modulePath, nodeModules));
-        for (const dep in deps) queue.defer(linkModule.bind(null, path.dirname(resolveSync(`${dep}/package.json`)), nodeModules));
+        queue.defer((cb) => linkModule(modulePath, nodeModules, (err) => cb(err)));
+        for (const dep in deps) queue.defer((cb) => linkModule(path.dirname(resolveSync(`${dep}/package.json`)), nodeModules, (err) => cb(err)));
         queue.await(cb);
       });
     });
     after((cb) => {
       const queue = new Queue();
-      queue.defer(unlinkModule.bind(null, modulePath, nodeModules));
-      for (const dep in deps) queue.defer(unlinkModule.bind(null, path.dirname(resolveSync(`${dep}/package.json`)), nodeModules));
+      queue.defer((cb) => unlinkModule(modulePath, nodeModules, (err) => cb(err)));
+      for (const dep in deps) queue.defer((cb) => unlinkModule(path.dirname(resolveSync(`${dep}/package.json`)), nodeModules, (err) => cb(err)));
       queue.await(cb);
     });
 
     describe('happy path', () => {
       it('link', (done) => {
-        link([], { cwd: dest }, (err?: Error): void => {
-          if (err) {
-            done(err);
-            return;
-          }
+        link([], { cwd: dest }, (err?: Error | null): void => {
+          if (err) return done(err);
+
           // Verify the module was linked
           const linkedPath = path.join(nodeModules, modulePackage.name);
           assert.ok(fs.existsSync(linkedPath) || fs.lstatSync(linkedPath).isSymbolicLink(), 'module should be linked');